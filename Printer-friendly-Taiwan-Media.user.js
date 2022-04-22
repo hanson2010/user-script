12de04aaf925955adbce83b328efb1eb42f351dc
Hanson Hu <h@pythoner.work>
2022-04-22 09:45:29 +0800

    Update Printer-friendly-Taiwan-Media.user.js
    支持**中央通訊社**和**聯合新聞網**。

--- Printer-friendly-Taiwan-Media.user.js
+++ Printer-friendly-Taiwan-Media.user.js
@@ -26,41 +26,98 @@
 
 GM_registerMenuCommand('Amend the page', onClick);
 
-function clearCna() {
-
-}
-
-function clearUdn() {
-    $('.header>.tools-box').remove();
-    $('.header>.navigation-wrapper').remove();
-    $('.breaking-news__wrapper').remove();
-    $('#ads-index-top--mobile').remove();
-
-    $('.wrapper-left>#ads-superBanner').remove();
-    $('.wrapper-left>#news_txtdown').remove();
-    $('.wrapper-left>#udn-520x290').remove();
-    $('.wrapper-left>#ads-Mobile_300x250--article').remove();
-    $('.wrapper-left>.article-content__ads--bd').remove();
-    $('.wrapper-left>.more-news').remove();
-    $('.wrapper-left>.related-news').remove();
-    $('.wrapper-left>.sponsor-ads').remove();
-    $('.wrapper-left>.ec-section').remove();
-    $('.wrapper-left>.facebook-comment').remove();
-    $('.wrapper-left>.discuss-board').remove();
-
-    $('.wrapper-aside').remove();
-
+function common() {
+    if (typeof jQuery != 'undefined') { 
+        console.log(jQuery.fn.jquery);
+    }
+    
+    $('script').remove();
+    $('noscript').remove();
     $('iframe').remove();
     $('hr').remove();
 }
 
+function clearCna() {
+    common();
+
+    $('.nav').remove();
+    $('.menu').remove();
+    $('.sideMenu').remove();
+    $('.instant').remove();
+    $('.TemasBlock').remove();
+    $('.pageContent').css('overflow', 'hidden');
+    $('.wrapper').css('width', '100%');
+    $('.overlayidle').remove();
+  
+    $('.article-feed').attr('data-next-urls-in-topic', []);
+    $('.article-feed').children().not(':first-child').remove();
+    $('article').css('padding', '0 20px');
+    $('article .centralContent').css('max-width', '100%');
+    $('article .centralContent .shareSide').remove();
+    $('article .centralContent .moreArticle').remove();
+    $('article .centralContent .BtnShareGroup').remove();
+    $('article .centralContent .appDownload').remove();
+    $('article .centralContent .articleADbox').remove();
+    $('article [class^="advertise"]').remove();
+    $('article .nextline').remove();
+
+    $('.jsADslot').remove();
+    $('footer').css('overflow', 'hidden');
+    $('footer > .f-wrapper').remove();
+  
+    window.infiniteScroll = new Function('console.log("abort");');
+}
+
+function clearUdn() {
+    common();
+
+    $('.udn-overlay').remove();
+    $('.udn-ads').remove();
+    $('.udn-idle').remove();
+    $('header .tools-box').remove();
+    $('header .navigation-wrapper').remove();
+    $('header .overlay-menu').remove();
+    $('header').css('border', 'none');
+  
+    $('main [class^="breaking-news_"]').remove();
+
+    $('.wrapper-left [id^="ads-"]').remove();
+    $('.wrapper-left [class^="article-content__ads-"]').remove();
+    $('.wrapper-left .article-content__audio').remove();
+    $('.wrapper-left .article-content__plugins').remove();
+    $('.wrapper-left .article-content__social').remove();
+    $('.wrapper-left .article-content__paragraph div').remove();
+    $('.wrapper-left [id^="taboola-"]').remove();
+    $('.wrapper-left .more-news').remove();
+    $('.wrapper-left .related-news').remove();
+    $('.wrapper-left .sponsor-ads').remove();
+    $('.wrapper-left .ec-section').remove();
+    $('.wrapper-left .facebook-comment').remove();
+    $('.wrapper-left .discuss-board').remove();
+    $('.wrapper-left').css('width', 'auto');
+    $('.wrapper-left .article-content__wrapper').css('padding', '0');
+    $('.wrapper-left .article-content__cover').css('--width', 'auto');
+    $('.wrapper-left .article-content__cover img').css('height', 'auto');
+    $('.wrapper-left .article-content').css('margin', '0');
+    $('.wrapper-left .article-content__paragraph').css({
+            'padding': '0',
+            'width': 'auto',
+        });
+
+    $('.wrapper-aside').remove();
+  
+    $('footer .footer-social').remove();
+    $('footer').css('border', 'none');
+    $('footer .container').css('padding', '0');
+    $('#gotop').remove();
+}
+
 function onClick() {
-    var siteName = $('meta[property="og:site_name"]').attr('content');
+    let host = window.location.host;
 
-    if (siteName.indexOf('中時新聞網') !== -1)
+    if (host.indexOf('cna') !== -1) {
         clearCna();
-
-    if (siteName.indexOf('聯合新聞網') !== -1)
+    } else if (host.indexOf('udn') !== -1) {
         clearUdn();
-
+    }
 }
